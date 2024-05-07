493dc1c89c28148e09cfd8f344b30822bb5344b6
Pavel Osipov <154364350+yaosipovpavel@users.noreply.github.com>
2024-05-07 22:28:21 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
-
-function getPartsFromFullname (){
 // Подключение файла array.php
 include 'array.php';
 $check = $example_persons_array[0]['fullname'];
+
+function getPartsFromFullname ($check){
+
 // Разъединение строки из массива
 $fname = explode(' ', $check);
+print_r($fname);
+
+getFullnameFromParts($fname);
+
+}
+
+
+function getFullnameFromParts ($fname){
+
     $surname = $fname[0];
     $name = $fname[1];
     $patronymic = $fname[2];
 
-    print_r($fname);
+    $new = "$surname $name $patronymic";
+    print_r($new);
+    }
 
-function getFullnameFromParts ($surname,$name,$patronymic){
-    getPartsFromFullname();
-$new = implode(' ', $surname,$name,$patronymic);
-print_r($surname);
-}
+getPartsFromFullname($check);
 
-}
-
-getPartsFromFullname();
 ?>
