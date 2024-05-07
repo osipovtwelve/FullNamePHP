38d326dda77ac8f77158bfbb188c7cb2950ed9ec
Pavel Osipov <154364350+yaosipovpavel@users.noreply.github.com>
2024-05-07 23:46:33 +0500

    Update index.php functions

--- index.php
+++ index.php
@@ -1,29 +1,38 @@
 <?php
 // Подключение файла array.php
 include 'array.php';
-$check = $example_persons_array[0]['fullname'];
 
-function getPartsFromFullname ($check){
+// Создание цикла, выводит элементы пока значение $i меньше значений в массиве, прибавляя каждый раз на 1
+for ($i = 0; $i < count($example_persons_array); $i++) {
+    $check = $example_persons_array[$i]['fullname'] . '<br>';
+    getPartsFromFullname($check);
+}
+/*
+getPartsFromFullname принимает как аргумент одну строку — склеенное ФИО. Возвращает как результат массив из трёх элементов с ключами ‘name’, ‘surname’ и ‘patronomyc’.
+Пример: как аргумент принимается строка «Иванов Иван Иванович», а возвращается массив [‘surname’ => ‘Иванов’ ,‘name’ => ‘Иван’, ‘patronomyc’ => ‘Иванович’].
+*/
 
-// Разъединение строки из массива
-$fname = explode(' ', $check);
-print_r($fname);
+function getPartsFromFullname($check)
+{
 
-getFullnameFromParts($fname);
-
+    // Разъединение строки из массива
+    $fname = explode(' ', $check);
+    // Запуск функции
+    getFullnameFromParts($fname);
+    // Вывод разделенного элементов массива
+    print_r($fname);
 }
 
+/*
+getFullnameFromParts принимает как аргумент три строки — фамилию, имя и отчество. Возвращает как результат их же, но склеенные через пробел.
+Пример: как аргументы принимаются три строки «Иванов», «Иван» и «Иванович», а возвращается одна строка — «Иванов Иван Иванович».
+*/
 
-function getFullnameFromParts ($fname){
+function getFullnameFromParts($fname)
+{
 
-    $surname = $fname[0];
-    $name = $fname[1];
-    $patronymic = $fname[2];
-
-    $new = "$surname $name $patronymic";
-    print_r($new);
-    }
-
-getPartsFromFullname($check);
-
-?>
+    // Склеивание элементов Фамилия[0] Имя[1] Отчество[2]
+    $new = "$fname[0] $fname[1] $fname[2] ";
+    // Вывод склееного имени
+    echo $new;
+}
