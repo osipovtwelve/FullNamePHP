ad9a8e0572867546d607e16655ddba04f9d64582
Pavel Osipov <154364350+yaosipovpavel@users.noreply.github.com>
2024-05-10 15:27:48 +0500

    Add function getShortName

--- index.php
+++ index.php
@@ -7,21 +7,8 @@ for ($i = 0; $i < count($example_persons_array); $i++) {
     $check = $example_persons_array[$i]['fullname'] . '<br>';
     getPartsFromFullname($check);
 }
-/*
-getPartsFromFullname принимает как аргумент одну строку — склеенное ФИО. Возвращает как результат массив из трёх элементов с ключами ‘name’, ‘surname’ и ‘patronomyc’.
-Пример: как аргумент принимается строка «Иванов Иван Иванович», а возвращается массив [‘surname’ => ‘Иванов’ ,‘name’ => ‘Иван’, ‘patronomyc’ => ‘Иванович’].
-*/
 
-function getPartsFromFullname($check)
-{
-
-    // Разъединение строки из массива
-    $fname = explode(' ', $check);
-    // Запуск функции
-    getFullnameFromParts($fname);
-    // Вывод разделенного элементов массива
-    print_r($fname);
-}
+// Объединение ФИО
 
 /*
 getFullnameFromParts принимает как аргумент три строки — фамилию, имя и отчество. Возвращает как результат их же, но склеенные через пробел.
@@ -35,4 +22,37 @@ function getFullnameFromParts($fname)
     $new = "$fname[0] $fname[1] $fname[2] ";
     // Вывод склееного имени
     echo $new;
+}
+
+// Разбиение ФИО
+
+/*
+getPartsFromFullname принимает как аргумент одну строку — склеенное ФИО. Возвращает как результат массив из трёх элементов с ключами ‘name’, ‘surname’ и ‘patronomyc’.
+Пример: как аргумент принимается строка «Иванов Иван Иванович», а возвращается массив [‘surname’ => ‘Иванов’ ,‘name’ => ‘Иван’, ‘patronomyc’ => ‘Иванович’].
+*/
+
+function getPartsFromFullname($check)
+{
+
+    // Разъединение строки из массива
+    $fname = explode(' ', $check);
+    // Запуск функции
+    getFullnameFromParts($fname);
+    getShortName($fname);
+    // Вывод разделенного элементов массива
+    print_r($fname);
+}
+
+
+// Сокращение ФИО
+
+function getShortName($fname)
+{
+    // Присваиваем Фамилию переменной $fam
+    $fam = "$fname[0]";
+    // Сокращаем значение переменной $fam до одного символа
+    $fam = mb_substr($fam[0], 0, 1);
+    // Присваиваем переменной $sokr Имя и сокращенную фамилию из переменной $fam
+    $sokr = "$fname[1] $fam.";
+    echo $sokr;
 }
